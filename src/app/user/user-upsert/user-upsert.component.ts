0c161955dce6006e873165dec1ea8a5005bc2cd4
GaneshK245 <ganeshmkande@gmail.com>
2024-01-16 23:04:05 +0530

    first commit

--- src/app/user/user-upsert/user-upsert.component.ts
+++ src/app/user/user-upsert/user-upsert.component.ts
@@ -111,3 +111,10 @@ export interface user {
   email: string;
   phone: number;
 }
+
+// git init
+// git add .
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/agurme555/lazyLoading.git
+// git push -u origin main 
